57b1a0a3ec69823fc449e598ebeb18815d293a0e
Kristmund Ryggstein <kristmunds@hotmail.com>
2020-12-22 09:33:54 +0000

    init git

---
+++ src/components/Dropzone.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react'
+import { Droppable, Draggable } from 'react-beautiful-dnd'
+import styled from 'styled-components'
+import PopupHero from './PopupHero'
+
+const Dropzone = ({ isDropDisabled, heroes, id, endGame, gameState, color }) => {
+  const [selectedHero, setSelectedHero] = useState("")
+
+  return (
+    <HeroContainerStyle>
+      <div style={{ marginTop: "20px", borderBottom: `${gameState === "review" ? `10px ${color} solid` : ""}` }}>{id}</div>
+      {endGame && heroes.length === 0 && (
+        <button className="btn btn-default" onClick={endGame}>
+          Enda spælið
+        </button>
+      )}
+      <Droppable droppableId={id} isDropDisabled={isDropDisabled}>
+        {(provided) => {
+          return (
+            <HeroSubContainerStyle
+              className="menu hero-list"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              grouped={id !== `Óflokkad` ? true : false}
+            >
+              {heroes.map(({ name, color, rank, description, comics }, index) => (
+                <Hero key={name} name={name} description={description} index={index}
+                  color={color} gameState={gameState} rank={rank}
+                  selectedHero={selectedHero}
+                  setSelectedHero={setSelectedHero}
+                  comics={comics}
+                />
+              ))}
+              {provided.placeholder}
+            </HeroSubContainerStyle>
+          )
+        }}
+      </Droppable>
+    </HeroContainerStyle>
+  )
+}
+const Hero = ({ name, color, rank, description, comics, index, gameState,
+  selectedHero, setSelectedHero }) => {
+  return (
+    <Draggable key={index} draggableId={name} index={index}>
+      {(provided) => {
+        return (
+          <HeroStyle
+            review={gameState === "review"}
+            color={color}
+            className="menu-item tile tile-centered center-column"
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+          >
+            <TextStyle onClick={() => {
+              setSelectedHero(name)
+            }} title={description} className="tile-content">
+              {name}
+            </TextStyle>
+            {gameState === "review" &&
+              <PopupHero name={name} rank={rank} description={description}
+                selectedHero={selectedHero} setSelectedHero={setSelectedHero}
+              />}
+          </HeroStyle>
+        )
+      }}
+    </Draggable>
+  )
+}
+
+const HeroContainerStyle = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  width: 430px;
+  margin: 20px;
+`
+const HeroStyle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  margin-top: 20px;
+  border: dotted black 1px;
+`
+
+const TextStyle = styled.div`
+  color: black;
+`
+
+const HeroSubContainerStyle = styled.div`
+`
+export default Dropzone
---
+++ src/custom/data.js
@@ -0,0 +1,24 @@
+export const COMICS = {
+  services: "Tænastur",
+  products: "Vørur"
+};
+
+export const HEROES = [
+  {
+    name: 'Ein vísindalig útrokning, sum vísir, hvussu nógv kann fiskast úr stovninum. Hetta tryggjar, at fiskastovnurin ikki minkar við tíðini, og hann kann endurnýggja seg. Kann fiskast á einum burðardyggum støði.',
+    comics: "Tænastur",
+    rank: 0,
+  },
+  {
+    name: 'Sambandið millum alt livandi tilfeingi á jørðini, og at djór eta hvørt annað. Um ein liður í føðiketuni er burtur, so ávirkar tað allan vegin upp gjøgnum føðiketuna.Hvør av øðrum liva má.',
+    comics: "Tænastur",
+    rank: 0,
+  },
+  {
+    name: 'Tá ið fiskað verður við ábyrgd. At tað ikki verður tikið ov nógv úr stovninum.',
+    comics: "Services",
+    rank: 0,
+  },
+
+
+];
